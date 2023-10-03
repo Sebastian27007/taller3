f1b90728f43f03bb72a32875bcaea660d30b3bcf
danielCotal <dcotal2021@alu.uct.cl>
2023-10-02 22:54:39 -0300

    Perfil v2 (en proceso)

---
+++ lib/SettingsApp.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppSettings extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.blue, Colors.lightGreenAccent],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        //color: Colors.purpleAccent,
+        height: size.height * 1.0,
+        child:const Text('Ajustes de la aplicación en proceso'),
+      ),
+    );
+  }
+
+}
