599e0ad06085d4402d94b623294e509610303d77
danielCotal <dcotal2021@alu.uct.cl>
2023-10-05 17:39:49 -0300

    contactos v2

--- lib/SettingsApp.dart
+++ lib/SettingsApp.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:ti3app/create_perfil.dart';
+
 
 class AppSettings extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contactos'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ), onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => UserPerfil()),
+            );
+          },
+        ),
+      ),
       body: Container(
         width: double.infinity,
         decoration: const BoxDecoration(
