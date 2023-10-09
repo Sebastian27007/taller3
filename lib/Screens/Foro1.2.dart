fafd44a874642be2b61451c788f934a09931ec1e
Nycto78 <gpesenti2019@alu.uct.cl>
2023-10-08 23:07:42 -0300

    Gonzalo
    Foro compilar

--- lib/Screens/Foro1.2.dart
+++ lib/Screens/Foro1.2.dart
@@ -32,3 +32,116 @@ class _HomePageWidgetState extends State<HomePageWidget>
       initialIndex: 0,
     )..addListener(() => setState(() {}));
   }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _model.unfocusNode.canRequestFocus
+          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+          : FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        appBar: AppBar(
+          backgroundColor: Color(0xFF0F65DD),
+          automaticallyImplyLeading: false,
+          leading: Align(
+            alignment: AlignmentDirectional(-0.92, -0.97),
+            child: Card(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              color: FlutterFlowTheme.of(context).secondaryBackground,
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(56),
+              ),
+              child: Container(
+                width: 120,
+                height: 120,
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                child: Image.network(
+                  'https://picsum.photos/seed/943/600',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          title: Align(
+            alignment: AlignmentDirectional(-1.00, 0.00),
+            child: Text(
+              'Comunidad\n',
+              style: FlutterFlowTheme.of(context).headlineMedium.override(
+                    fontFamily: 'Outfit',
+                    color: Colors.white,
+                    fontSize: 22,
+                  ),
+            ),
+          ),
+          actions: [],
+          centerTitle: false,
+          elevation: 2,
+        ),
+        body: SafeArea(
+          top: true,
+          child: Stack(
+            children: [
+              Stack(
+                children: [
+                  Align(
+                    alignment: AlignmentDirectional(0.00, 0.00),
+                    child: Container(
+                      width: 395,
+                      height: 619,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xFF94C3F9),
+                            Color(0xFF7130F0),
+                            Color(0xF13420FD)
+                          ],
+                          stops: [0.2, 0.4, 0.4],
+                          begin: AlignmentDirectional(1, 0.98),
+                          end: AlignmentDirectional(-1, -0.98),
+                        ),
+                      ),
+                      child: Align(
+                        alignment: AlignmentDirectional(0.00, 0.00),
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment(0, 0),
+                              child: TabBar(
+                                labelColor: Colors.white,
+                                unselectedLabelColor: Colors.white,
+                                labelStyle: FlutterFlowTheme.of(context)
+                                    .titleSmall
+                                    .override(
+                                      fontFamily: 'Readex Pro',
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                unselectedLabelStyle:
+                                    FlutterFlowTheme.of(context).titleSmall,
+                                indicatorColor:
+                                    FlutterFlowTheme.of(context).primary,
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+                                tabs: [
+                                  Tab(
+                                    text: 'Importantes',
+                                  ),
+                                  Tab(
+                                    text: 'Otra noticias',
+                                  ),
+                                ],
+                                controller: _model.tabBarController,
+                                  ),
+                            ),
