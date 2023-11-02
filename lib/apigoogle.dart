0f8bbf13dd512357e8dd9b78fe6d887ad68e0719
Sebastian27007 <sebastian.riquelme2019@alu.uct.cl>
2023-11-02 19:47:41 -0300

    Avance eventos calendar

--- lib/apigoogle.dart
+++ lib/apigoogle.dart
@@ -1,22 +1,52 @@
+import 'package:flutter/material.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:googleapis/calendar/v3.dart' as calendar;
+import 'package:url_launcher/url_launcher.dart';
 
 const _clientId = "290325605957-2csp29v4b48qahc1qt2bu4qgugol20rt.apps.googleusercontent.com";
 const _clientSecret = "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";
 
 void main() {
-  var clientID = ClientId(_clientId, _clientSecret);
-  var scopes = [calendar.CalendarApi.calendarScope];
-
-  clientViaUserConsent(clientID, scopes, prompt)
-      .then((AuthClient client) {
-    var calendarApi = calendar.CalendarApi(client);
-    // Ahora puedes usar calendarApi para interactuar con la API de Google Calendar
-  });
+  runApp(MyApp());
 }
 
 void prompt(String url) {
   print("Por favor ve a la siguiente URL y concede acceso a la aplicación:");
   print("  => $url");
   print("");
+}
+
+void _openURL() async {
+  var clientID = ClientId(_clientId, _clientSecret);
+  var scopes = [calendar.CalendarApi.calendarScope];
+
+  var url = await clientViaUserConsent(clientID, scopes, prompt);
+
+  // Abre la URL en el navegador interno de la aplicación
+  if (await canLaunch(url)) {
+    await launch(url, forceSafariVC: false, forceWebView: false);
+  } else {
+    throw 'No se puede abrir $url';
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Google Calendar API'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              _openURL();
+            },
+            child: Text('Authorize Google Calendar Access'),
+          ),
+        ),
+      ),
+    );
+  }
 }
