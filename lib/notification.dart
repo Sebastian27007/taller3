4d9553c22eceb459307eb2dfd79da9b8ce3cf2f5
ZeRSToReR1312 <cuevasramirez360@gmail.com>
2023-11-06 15:24:16 -0300

    Enviar correo electronico
    Envio de correo electronico al ocurrir un evento

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,35 +1,85 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-
-Future<void> _configureFirebaseMessaging() async {
-  _firebaseMessaging.configure(
-    onMessage: (Map<String, dynamic> message) async {
-      print("onMessage: $message");
-    },
-    onLaunch: (Map<String, dynamic> message) async {
-      print("onLaunch: $message");
-    },
-    onResume: (Map<String, dynamic> message) async {
-      print("onResume: $message");
-    },
-  );
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
 }
 
-Future<void> _requestNotificationPermissions() async {
-  NotificationSettings settings = await _firebaseMessaging.requestPermission(
-    alert: true,
-    badge: true,
-    provisional: false,
-    sound: true,
-  );
+class MyHomePage extends StatelessWidget {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
-  print("User granted permission: ${settings.authorizationStatus}");
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Notifications and Email'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            sendNotification();
+            sendEmail();
+          },
+          child: Text('Presione'),
+        ),
+      ),
+    );
+  }
 
-String? _deviceToken;
+  void sendNotification() async {
+    var android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOS = IOSInitializationSettings();
+    var initSettings = InitializationSettings(android: android, iOS: iOS);
+    await flutterLocalNotificationsPlugin.initialize(initSettings);
 
-Future<void> _getDeviceToken() async {
-  _deviceToken = await _firebaseMessaging.getToken();
-  print("Device Token: $_deviceToken");
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      '',
+      '',
+      '',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Notification Title',
+      'Notification Body',
+      platformChannelSpecifics,
+      payload: 'New Payload',
+    );
+  }
+
+  void sendEmail() async {
+    String username = 'cuevasramirez360@gmail.com';
+    String password = '';
+
+    final smtpServer = gmail(username, password);
+
+    final message = Message()
+      ..from = Address(username, 'Martin')
+      ..recipients.add('recipient@gmail.com')
+      ..subject = 'Test Dart Mailer library :: 😀 :: ${DateTime.now()}'
+      ..text = 'This is the plain text.\nThis is line 2 of the text part.'
+      ..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>";
+
+    try {
+      await send(message, smtpServer);
+      print('Email sent!');
+    } catch (e) {
+      print('Error occurred while sending email: $e');
+    }
+  }
 }
