fd755043584eea13c17bdece0828ae00ebae4e1a
KarenEvelyn <r3e.bl4ck.skull@gmail.com>
2023-11-11 18:12:41 -0300

    asesores y cursos
    funcional

--- lib/Course.dart
+++ lib/Course.dart
@@ -1,51 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:ti3app/main.dart';
+import 'package:ti3app/advisor.dart';
 
-class Course {
-  final String title;
-  final String description;
-  bool isEnrolled;
-
-  Course(this.title, this.description, this.isEnrolled);
+void main() {
+  runApp(MyApp());
 }
 
-class CoursePage extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _CoursePageState createState() => _CoursePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inscripción de Cursos',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: CoursesPage(),
+    );
+  }
 }
 
-class _CoursePageState extends State<CoursePage> {
-  List<Course> courses = [
-    Course('Curso 1', 'Descripción del curso 1', false),
-    Course('Curso 2', 'Descripción del curso 2', false),
-    Course('Curso 3', 'Descripción del curso 3', false),
-    Course('Curso 4', 'Descripción del curso 4', false),
-    Course('Curso 5', 'Descripción del curso 5', false),
-    Course('Curso 6', 'Descripción del curso 6', false),
+class CoursesPage extends StatelessWidget {
+  final List<Course> courses = [
+    Course(
+      name: 'CONOCIENDO EL MODELO EDUCATIVO UC TEMUCO',
+      description: 'Modalidad Autoinstruccional',
+      url: 'https://dte.uct.cl/formaciondocente2023/',
+      color: Colors.green,
+    ),
+    Course(
+      name: 'INTRODUCCIÓN A EDUCA BLACKBOARD',
+      description: 'Modalidad Autoinstruccional',
+      url: 'https://dte.uct.cl/formaciondocente2023/',
+      color: Colors.blueAccent,
+    ),
+    Course(
+      name: 'DOCENCIA EN LÍNEA',
+      description: 'Modalidad Full E-learning',
+      url: 'https://dte.uct.cl/formaciondocente2023/',
+      color: Colors.green,
+    ),
+    Course(
+      name: 'INDUCCCIÓN A DOCENTES NUEVOS',
+      description: 'Modalidad Autoinstruccional',
+      url: 'https://dte.uct.cl/formaciondocente2023/',
+      color: Colors.blue,
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inscripción Cursos'),
+        title: Text('Inscripción de Cursos'),
       ),
       body: ListView.builder(
         itemCount: courses.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(courses[index].title),
-            subtitle: Text(courses[index].description),
-            trailing: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  courses[index].isEnrolled = true;
-                });
-              },
-              child: Text(courses[index].isEnrolled ? 'Inscrito' : 'Inscribirse'),
+
+          return Container(
+            width: 200,
+            height: 140,
+            color: Colors.blueAccent,
+            child: ListTile(
+              title: Text(courses[index].name),
+              subtitle: Text(courses[index].description),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  launchURL(courses[index].url);
+                },
+                child: Text('Ir al curso'),
+              ),
             ),
+
           );
         },
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          onTap: (index) {
+            if (index == 0) {
+              Navigator.push(context,MaterialPageRoute(builder: (context) => MyCombinedPage()));
+              //navegar home
+            } else if (index == 1) {
+              Navigator.push(context,MaterialPageRoute(builder: (context) => AdvisorsPage()));
+              //navegar asesores
+            }
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Inicio',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people),
+              label: 'Asesores',
+            ),
+          ],
+          selectedItemColor: Colors.blue
+      ),
     );
   }
+
+  void launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'No se pudo abrir el enlace $url';
+    }
+  }
+}
+
+class Course {
+  final String name;
+  final String description;
+  final String url;
+  final Color color;
+
+  Course({
+    required this.name,
+    required this.description,
+    required this.url,
+    required this.color,
+  });
 }
